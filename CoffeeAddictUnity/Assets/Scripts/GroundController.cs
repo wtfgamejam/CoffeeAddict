1c8c81d4356899eadef1ee668dca3af1ea315b32
Amesh Narsing <ameshn@gamehouse.com>
2015-05-18 20:01:46 -0700

    Building colours

--- CoffeeAddictUnity/Assets/Scripts/GroundController.cs
+++ CoffeeAddictUnity/Assets/Scripts/GroundController.cs
@@ -9,10 +9,16 @@ public class GroundController : MonoBehaviour {
 
 	public int CollectableChance = 1;
 
+	public SpriteRenderer rend;
+
+	public Material[] materials;
+
+	public int order = 0;
+
 	// Use this for initialization
 	void Awake () {
 		int chance = Random.Range(1,CollectableChance+1);
-		float collectableOffsetX = Random.Range(-5f,5f);
+		float collectableOffsetX = Random.Range(-2f,2f);
 		if(chance == CollectableChance)
 		{
 			Vector3 position = CollectableLocation.transform.position;
@@ -20,6 +26,10 @@ public class GroundController : MonoBehaviour {
 			GameObject collect = Instantiate(Collectable, position, Quaternion.identity) as GameObject;
 			collect.transform.parent = CollectableLocation.transform;
 		}
+
+		int matChance = Random.Range(0, materials.Length +1);
+		rend.material = materials[matChance];
+		rend.sortingOrder = order;
 	}
 
 }
--- CoffeeAddictUnity/Assets/Scripts/Spawner.cs
+++ CoffeeAddictUnity/Assets/Scripts/Spawner.cs
@@ -6,6 +6,7 @@ public class Spawner : MonoBehaviour {
 	public GameObject[] obj;
 	public float SpawnMin = 1f;
 	public float SpawnMax = 2f;
+	public int order = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -17,6 +18,8 @@ public class Spawner : MonoBehaviour {
 		if(this.enabled)
 		{
 			GameObject go = obj[Random.Range(0, obj.Length)];
+			GroundController ground = go.GetComponent<GroundController>();
+			ground.order = order;
 			Instantiate(go, transform.position, Quaternion.identity);
 
 			Invoke("Spawn", Random.Range(this.SpawnMin, this.SpawnMax));
